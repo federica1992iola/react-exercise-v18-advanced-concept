4f5a14c42ddc2a42f12af10e28d6b999a28abaa1
federica1992iola <f3xambientalist@hotmail.it>
2025-02-05 19:31:11 +0100

    2.

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -4,6 +4,6 @@ import { App } from './App';
 import './style.scss';
 
 const rootElement: HTMLElement | null = document.getElementById('root');
-const root: Root = createRoot(rootElement as HTMLElement);
+const root = createRoot(rootElement as HTMLElement);
 
 root.render(<App />);
